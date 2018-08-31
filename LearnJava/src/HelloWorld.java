e6a6ed73bd670172a3120eab87a071c9e02bc338
alex <alex@10.0.0.203>
2018-08-31 15:35:06 -0500

    commit by alex

---
+++ LearnJava/src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+	public static void main(String args[]) {
+		System.out.println("Hello! My name is Alex!");
+		System.out.println("I have been coding for 8 Years now.");
+		System.out.println("I've taken a couple courses for coding.");
+		System.out.println("I've dabbled with android studio and I know most of the basics.");
+	}
+}
