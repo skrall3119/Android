9116278c3755c029fa4a1b1d09722eb348349f3f
alex <alex@10.0.0.203>
2018-09-02 15:46:26 -0500

    variables

---
+++ LearnJava/src/variables.java
@@ -0,0 +1,22 @@
+
+public class variables {
+	static void main() {
+		int var1 = 2;
+		short var2 = 1;
+		byte var3 = 0;
+		long var4 = 1000000000;
+		char var5 = 'a';
+		float var6 = 12.231123f;
+		double var7 = 6.4;
+		boolean var8 = true;
+		
+		System.out.println(var1 + " is an Integer");
+		System.out.println(var2 + " is a Short");
+		System.out.println(var3 + " is a Byte");
+		System.out.println(var4 + " is a Long");
+		System.out.println(var5 + " is a Char");
+		System.out.println(var6 + " is a Float");
+		System.out.println(var7 + " is a double");
+		System.out.println(var8 + " is a Boolean");
+	}
+}
