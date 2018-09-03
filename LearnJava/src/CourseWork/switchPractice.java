d39254f47403e60a0aa8fa86e4bf1fa1e771663f
alex <alex@10.0.0.203>
2018-09-03 15:18:22 -0500

    added SwitchPractice

--- LearnJava/src/CourseWork/switchPractice.java
+++ LearnJava/src/CourseWork/switchPractice.java
@@ -1,10 +1,44 @@
+//VARIABLES: number, Integer; numberName, String;
+
 package CourseWork;
+import java.util.Scanner;
 
 public class switchPractice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int number;
+		String numberName;
+		Scanner scanner = new Scanner(System.in);
 
+		System.out.println("Number name translator to spanish!");
+		System.out.println("Please enter a number between 1 and 10: ");
+		number = scanner.nextInt();
+		scanner.close();
+		
+		switch (number) {
+			case 1: numberName = "Uno";
+			break;
+			case 2: numberName = "Dos";
+			break;
+			case 3: numberName = "Tres";
+			break;
+			case 4: numberName = "Quatro";
+			break;
+			case 5: numberName = "Cinco";
+			break;
+			case 6: numberName = "Seis";
+			break;
+			case 7: numberName = "Siete";
+			break;
+			case 8: numberName = "Ocho";
+			break;
+			case 9: numberName = "Nueve";
+			break;
+			case 10: numberName = "Dies";
+			break;
+			default: numberName = "That is not a valid entry. Please enter a number between 1 and 10";
+		}
+		System.out.println(numberName);
 	}
 
 }
