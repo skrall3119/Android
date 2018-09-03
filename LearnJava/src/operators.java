8940d76e554bdb27939f047cca21ca21011db6ac
alex <alex@10.0.0.203>
2018-09-03 13:57:29 -0500

    added operators.java

---
+++ LearnJava/src/operators.java
@@ -0,0 +1,10 @@
+//plan: perform basic mathematical functions using +, -, *, /, and %, and displsy them via console.
+public class operators {
+	public static void main(String args[]) {
+		System.out.println("10 + 10 = " + (10 + 10));
+		System.out.println("15 - 12 = " + (15 - 12));
+		System.out.println("6 * 8 = " + (6 * 8));
+		System.out.println("72 / 8 = " + (72 / 8));
+		System.out.println("128 % 16 = " + (128 % 16));
+	}
+}
