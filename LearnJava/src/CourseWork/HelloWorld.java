71da9d9b87e5781823cf15bee56556da46847c66
alex <alex@10.0.0.203>
2018-09-05 13:28:29 -0500

    re-added HelloWorld.java.

---
+++ LearnJava/src/CourseWork/HelloWorld.java
@@ -0,0 +1,12 @@
+package CourseWork;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello! My name is Alex.");
+		System.out.println("I am 20 years old, and I first learned how to code in 7th grade.");
+		System.out.println("I am incredibly excited to begin learning Android Studio as well as Xcode.");
+		System.out.println("I can't wait to see what I am going to learn in the mobile development courses.");
+	}
+
+}
