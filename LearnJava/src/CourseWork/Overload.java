fa13b9300eff0967ae73dbf159ded657169780e3
alex <alex@10.0.0.203>
2018-09-12 14:23:04 -0500

    added Overload

---
+++ LearnJava/src/CourseWork/Overload.java
@@ -0,0 +1,47 @@
+package CourseWork;
+
+import java.util.Scanner;
+
+public class Overload {
+	
+	public static void printInfo(String name, String number, String businessname) {
+		System.out.println(name);
+		System.out.println(number);
+		System.out.println(businessname);
+	}
+	
+	public static void printInfo(String name, String number) {
+		System.out.println(name);
+		System.out.println(number);
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Are you a business customer or residential customer?");
+		String firstChoice = scanner.nextLine();
+		
+		String name;
+		String number;
+		
+		firstChoice = firstChoice.toLowerCase();
+		
+		switch(firstChoice) {
+		case "business":
+			System.out.print("Please enter a Business name: ");
+			String businessName = scanner.nextLine();
+			System.out.print("Please enter your name: ");
+			name = scanner.nextLine();
+			System.out.print("Please enter a phone number: ");
+			number = scanner.nextLine();
+			printInfo(name, number, businessName);
+		case "resident":
+			System.out.print("Please enter your name: ");
+			name = scanner.nextLine();
+			System.out.print("Please enter a phone number: ");
+			number = scanner.nextLine();
+			printInfo(name, number);
+		}
+		scanner.close();
+	}
+
+}
