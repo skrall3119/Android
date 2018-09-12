6136afbf1ccef484a6f9e0982afc74d046db866f
alex <alex@10.0.0.203>
2018-09-12 15:19:54 -0500

    added array program

---
+++ LearnJava/src/CourseWork/array.java
@@ -0,0 +1,12 @@
+package CourseWork;
+
+public class array {
+
+	public static void main(String[] args) {
+		String[] supplies = {"Ice cream", "Cones", "Dishes", "Sprinkles", "Chocolate"};
+		for(int i = 0; i < supplies.length; i++) {
+			System.out.println(supplies[i]);
+		}
+	}
+
+}
