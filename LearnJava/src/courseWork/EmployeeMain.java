0ed8046cf61b089005a2fd6febbb6435b8729653
alex <alex@10.0.0.203>
2018-09-25 23:32:38 -0500

    added main class for Employee

---
+++ LearnJava/src/courseWork/EmployeeMain.java
@@ -0,0 +1,15 @@
+package courseWork;
+
+public class EmployeeMain {
+	
+	public static void main(String args[]) {
+		Employee Alex = new Employee("Alex", 333, 3);
+		Employee Conor = new Employee("Conor", 117, 2);
+		Employee Zack = new Employee("Zack", 223, 1);
+		
+		System.out.println(Alex);
+		System.out.println(Conor);
+		System.out.println(Zack);
+		
+	}
+}
